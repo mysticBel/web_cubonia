bf2e03d5308b63b0854ed1e2734ea7b5d57035b3
mysticBel <dnxbel@gmail.com>
2024-02-24 10:30:24 -0500

    add : navbar responsive and desktop works with jquery

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// navbar
+$(document).ready(function() {
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll > 100) {
+        $("nav").css("background-color", "#000"); /* Cambiar a fondo negro al hacer scroll */
+      } else {
+        $("nav").css("background-color", "rgba(0, 0, 0, 0.3)"); /* Restaurar el fondo con transparencia */
+      }
+    });
+
+    $(".menu-icon").click(function() {
+      $("nav ul").toggleClass("show");
+    });
+  });
 
 // swipper
 
@@ -45,4 +60,61 @@ $(".carousel-control-next").on("click", function () {
 
   function irAEnlace(url) {
     window.location.href = url;
+  }
+
+
+  //// slider coursos vanilla
+  let slideIndex = 0;
+  let isDragging = false;
+  let startX, currentX;
+
+  $(document).ready(function() {
+    $('.next-btn').click(function() {
+      moverSlider(1);
+    });
+
+    $('.prev-btn').click(function() {
+      moverSlider(-1);
+    });
+
+    $('.cards-slider').on('mousedown touchstart', function(e) {
+      isDragging = true;
+      startX = e.pageX || e.originalEvent.touches[0].pageX;
+    });
+
+    $(document).on('mouseup touchend', function() {
+      isDragging = false;
+    });
+
+    $('.cards-slider').on('mousemove touchmove', function(e) {
+      if (!isDragging) return;
+      e.preventDefault();
+      currentX = e.pageX || e.originalEvent.touches[0].pageX;
+      const diffX = currentX - startX;
+      if (diffX > 50) {
+        moverSlider(-1);
+        isDragging = false;
+      } else if (diffX < -50) {
+        moverSlider(1);
+        isDragging = false;
+      }
+    });
+  });
+
+  function moverSlider(n) {
+    slideIndex += n;
+    const maxSlideIndex = $('.card').length - 3; // Definir el máximo índice del slider
+    if (slideIndex > maxSlideIndex) {
+      slideIndex = maxSlideIndex;
+    }
+    if (slideIndex < 0) {
+      slideIndex = 0;
+    }
+    mostrarCards();
+  }
+
+  function mostrarCards() {
+    const slideWidth = $('.card').width();
+    const offset = -slideIndex * slideWidth;
+    $('.cards-slider').css('transform', 'translateX(' + Math.min(0, Math.max(offset, -($('.card').length - 3) * slideWidth)) + 'px)');
   }
