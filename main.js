e796aa6bc99b96ac9b7843ec6abf4f1c9ccb9d09
mysticBel <dnxbel@gmail.com>
2024-02-26 07:20:48 -0500

    add : principal background , responsive without styles

--- main.js
+++ main.js
@@ -14,31 +14,6 @@ $(document).ready(function() {
     });
   });
 
-// swipper
-
-var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-var cardWidth = $(".carousel-item").width();
-
-var scrollPosition = 0;
-
-$(".carousel-control-next").on("click", function () {
-    if (scrollPosition < (carouselWidth - cardWidth * 4)) { 
-        console.log("next")
-        //check if you can go any further
-      scrollPosition += cardWidth;  //update scroll position
-      $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
-    }
-  });
-
-  $(".carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
-      scrollPosition -= cardWidth;
-      $(".carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
 
 
   /////////
@@ -63,63 +38,7 @@ $(".carousel-control-next").on("click", function () {
   }
 
 
-  //// slider coursos vanilla
-  let slideIndex = 0;
-  let isDragging = false;
-  let startX, currentX;
-
-  $(document).ready(function() {
-    $('.next-btn').click(function() {
-      moverSlider(1);
-    });
-
-    $('.prev-btn').click(function() {
-      moverSlider(-1);
-    });
-
-    $('.cards-slider').on('mousedown touchstart', function(e) {
-      isDragging = true;
-      startX = e.pageX || e.originalEvent.touches[0].pageX;
-    });
-
-    $(document).on('mouseup touchend', function() {
-      isDragging = false;
-    });
-
-    $('.cards-slider').on('mousemove touchmove', function(e) {
-      if (!isDragging) return;
-      e.preventDefault();
-      currentX = e.pageX || e.originalEvent.touches[0].pageX;
-      const diffX = currentX - startX;
-      if (diffX > 50) {
-        moverSlider(-1);
-        isDragging = false;
-      } else if (diffX < -50) {
-        moverSlider(1);
-        isDragging = false;
-      }
-    });
-  });
-
-  function moverSlider(n) {
-    slideIndex += n;
-    const maxSlideIndex = $('.card').length - 3; // Definir el máximo índice del slider
-    if (slideIndex > maxSlideIndex) {
-      slideIndex = maxSlideIndex;
-    }
-    if (slideIndex < 0) {
-      slideIndex = 0;
-    }
-    mostrarCards();
-  }
-
-  function mostrarCards() {
-    const slideWidth = $('.card').width();
-    const offset = -slideIndex * slideWidth;
-    $('.cards-slider').css('transform', 'translateX(' + Math.min(0, Math.max(offset, -($('.card').length - 3) * slideWidth)) + 'px)');
-  }
-
-
+  
 // form 
 document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault();
