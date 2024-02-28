1bcca743bde85b8d21c16c7a3173b181c70910f4
Cindy Mendoza <mendozaic17@gmail.com>
2024-02-28 14:45:59 -0500

    implementacion de slick slider

--- main.js
+++ main.js
@@ -1,56 +1,67 @@
 // navbar
-$(document).ready(function() {
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      if (scroll > 100) {
-        $("nav").css("background-color", "#000"); /* Cambiar a fondo negro al hacer scroll */
-      } else {
-        $("nav").css("background-color", "rgba(0, 0, 0, 0.3)"); /* Restaurar el fondo con transparencia */
-      }
-    });
-
-    $(".menu-icon").click(function() {
-      $("nav ul").toggleClass("show");
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 100) {
+      $("nav").css(
+        "background-color",
+        "#000"
+      ); /* Cambiar a fondo negro al hacer scroll */
+    } else {
+      $("nav").css(
+        "background-color",
+        "rgba(0, 0, 0, 0.3)"
+      ); /* Restaurar el fondo con transparencia */
+    }
   });
 
-
-
-  /////////
-  function mostrarInformacion(numero) {
-    // Ocultar todas las informaciones de personas
-    document.querySelectorAll('.person-info').forEach(info => {
-      info.style.display = 'none';
-    });
-    // Mostrar la información de la persona seleccionada
-    document.getElementById('info-' + numero).style.display = 'flex';
-    
-    // Eliminar la clase 'active' de todas las imágenes
-    document.querySelectorAll('.slider img').forEach(img => {
-      img.classList.remove('active');
-    });
-    // Agregar la clase 'active' a la imagen seleccionada
-    document.querySelector('.slider img:nth-child(' + numero + ')').classList.add('active');
-  }
-
-  function irAEnlace(url) {
-    window.location.href = url;
-  }
-
-
+  $(".menu-icon").click(function () {
+    $("nav ul").toggleClass("show");
+  });
+  $(".slider").slick({
+    dots: true,
+    arrows:true,
+    speed: 500
+  });
   
-// form 
-document.getElementById("contactForm").addEventListener("submit", function(event) {
+});
+
+/////////
+function mostrarInformacion(numero) {
+  // Ocultar todas las informaciones de personas
+  document.querySelectorAll(".person-info").forEach((info) => {
+    info.style.display = "none";
+  });
+  // Mostrar la información de la persona seleccionada
+  document.getElementById("info-" + numero).style.display = "flex";
+
+  // Eliminar la clase 'active' de todas las imágenes
+  document.querySelectorAll(".slider img").forEach((img) => {
+    img.classList.remove("active");
+  });
+  // Agregar la clase 'active' a la imagen seleccionada
+  document
+    .querySelector(".slider img:nth-child(" + numero + ")")
+    .classList.add("active");
+}
+
+function irAEnlace(url) {
+  window.location.href = url;
+}
+
+// form
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
     //event.preventDefault();
     var nombre = document.getElementById("nombre").value;
     var email = document.getElementById("email").value;
     var mensaje = document.getElementById("mensaje").value;
 
     if (nombre && email && mensaje) {
-        alert("Mensaje enviado con éxito.");
-        // Aquí podrías enviar los datos del formulario a tu servidor o realizar alguna otra acción
+      alert("Mensaje enviado con éxito.");
+      // Aquí podrías enviar los datos del formulario a tu servidor o realizar alguna otra acción
     } else {
-        alert("Completa los campos.");
+      alert("Completa los campos.");
     }
-});
-
+  });
